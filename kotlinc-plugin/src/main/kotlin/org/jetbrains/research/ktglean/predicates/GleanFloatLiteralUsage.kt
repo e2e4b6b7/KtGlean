435a3fcd5da4030656c59118df5277e8441582a5
Roman Venediktov <e2e4b6b7@gmail.com>
2022-04-22 15:00:23 +0300

    Fixes

--- kotlinc-plugin/src/main/kotlin/org/jetbrains/research/ktglean/predicates/GleanFloatLiteralUsage.kt
+++ kotlinc-plugin/src/main/kotlin/org/jetbrains/research/ktglean/predicates/GleanFloatLiteralUsage.kt
@@ -5,5 +5,5 @@ import org.jetbrains.research.ktglean.serialization.Fact
 data class GleanFloatLiteralUsage(override val key: Key) : Fact {
     override val name: String get() = "floatLiteralAnalysis.FloatLiteralUsage.1"
 
-    data class Key(val call: GleanFunctionCall, val literal: String, val pos: Int)
+    data class Key(val call: GleanFunctionCall, val literal: String, val position: Int)
 }
